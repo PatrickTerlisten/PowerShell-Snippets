1b31c1f6a602cf9baa2cf66c90adfabb959cac97
Patrick Terlisten <patrick@blazilla.de>
2017-04-16 10:27:44 +0200

    ? to Where-Object

--- Find files with missing prefix.ps1
+++ Find files with missing prefix.ps1
@@ -1,6 +1,6 @@
 # This snippets searches for files with a missing prefix (in this case yyyyMMdd), takes the creation date of the file and adds this to the filename
 
-Get-Childitem "C:\Users\p.terlisten\Documents" -recurse | ? {$_.FullName -notlike "*Vorlagen*"} | Get-ChildItem -File | ? {$_.name -notmatch '^\d{8}'} | ForEach-Object {
+Get-Childitem "C:\Users\p.terlisten\Documents" -recurse | Where-Object {$_.FullName -notlike "*Vorlagen*"} | Get-ChildItem -File | Where-Object {$_.name -notmatch '^\d{8}'} | ForEach-Object {
 
     $creationtime = $_.CreationTime
     $newdate = $creationtime.ToString("yyyyMMdd")
